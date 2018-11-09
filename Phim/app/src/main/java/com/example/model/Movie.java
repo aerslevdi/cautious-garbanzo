38ea8e192005edb1297752430f51316978052b18
Cristal Roberts Alcorta <cgrobertsalcorta@yahoo.com>
2018-11-09 01:37:43 -0300

    DAOMovies

--- Phim/app/src/main/java/com/example/model/Movie.java
+++ Phim/app/src/main/java/com/example/model/Movie.java
@@ -1,44 +1,56 @@
 package com.example.model;
 
+import java.util.Date;
 import java.util.List;
 
 public class Movie {
 
     //ATRIBUTOS
-    private Integer id;
+
     private String title;
     private String year;
     private String rated;
+    private String fecha;
+    private String duration;
     private String genre;
     private String director;
     private String writers;
     private List<ActorDetalle> actores;
     private String plot;
+    private String language;
     private String awards;
     private String countryOrigin;
-    private String metaScore;
-    private String duration;
-    private Integer image;
+    private Integer poster;
     private String imbdScore;
+    private String metaScore;
+    private String votes;
+    private Integer id;
+    private String format;
+    private Integer image;
 
     //CONSTRUCTOR
 
 
-    public Movie(Integer id, String title, String year, String rated, String genre, String director, String writers, List<ActorDetalle> actores, String plot, String awards, String countryOrigin, String metaScore, String duration, Integer image, String imbdScore) {
+    public Movie( String title, String year, String rated, String fecha, String duration, String genre, String director, String writers, List<ActorDetalle> actores, String plot, String language, String awards, String countryOrigin, Integer poster, String imbdScore, String metaScore, String votes, Integer id, String format,  Integer image) {
         this.id = id;
         this.title = title;
         this.year = year;
         this.rated = rated;
+        this.fecha = fecha;
+        this.duration = duration;
         this.genre = genre;
         this.director = director;
         this.writers = writers;
         this.actores = actores;
         this.plot = plot;
+        this.language = language;
         this.awards = awards;
         this.countryOrigin = countryOrigin;
+        this.poster = poster;
         this.metaScore = metaScore;
-        this.duration = duration;
+        this.votes = votes;
         this.image = image;
+        this.format = format;
         this.imbdScore = imbdScore;
     }
 
@@ -61,8 +73,20 @@ public class Movie {
         return rated;
     }
 
+    public String getFecha() {
+        return fecha;
+    }
+
     public String getGenre() {
         return genre;
+    }
+
+    public String getLanguage() {
+        return language;
+    }
+
+    public String getFormat() {
+        return format;
     }
 
     public String getDirector() {
@@ -89,8 +113,16 @@ public class Movie {
         return countryOrigin;
     }
 
+    public Integer getPoster() {
+        return poster;
+    }
+
     public String getMetaScore() {
         return metaScore;
+    }
+
+    public String getVotes() {
+        return votes;
     }
 
     public String getDuration() {
