771c7422b8fd423f0b2bbad7584fbf8e078c6a30
aerslevdi <cgrobertsalcorta@yahoo.com.ar>
2018-11-16 16:43:55 -0300

    Todo roto

--- Phim/app/src/main/java/com/example/dao/MoviesDao.java
+++ Phim/app/src/main/java/com/example/dao/MoviesDao.java
@@ -19,6 +19,7 @@ public class MoviesDao {
     List<Pelicula> peliculasAventura = new ArrayList();
     List<Pelicula> peliculasSeries = new ArrayList();
     List<Categoria> categorias = new ArrayList();
+    DAOActores daoActores = new DAOActores();
 
 
     private static MoviesDao instance;
@@ -40,13 +41,13 @@ public class MoviesDao {
 
 
     private void categoriaInfantiles(){
-        peliculasInfantiles.add(new Pelicula("Finding Nemo", 1, R.drawable.movie1infantiles));
-        peliculasInfantiles.add(new Pelicula("High Scholl Musical",1, R.drawable.movie2infantiles));
-        peliculasInfantiles.add(new Pelicula("Maleficcent", 1, R.drawable.movie3infantiles));
-        peliculasInfantiles.add(new Pelicula("Los Increibles 2",1,  R.drawable.movie4infantiles));
-        peliculasInfantiles.add(new Pelicula("Los Increibles", 1, R.drawable.movie5infantiles));
-        peliculasInfantiles.add(new Pelicula("Intensamente", 1,R.drawable.movie6infantiles));
-        peliculasInfantiles.add(new Pelicula("Gemelas", 1,R.drawable.movie7infantiles));
+        peliculasInfantiles.add(new Pelicula("Finding Nemo", "2003","PG","30-05-2003", "100 min", "Animation","Andrew Stanton", "Andrew Stanton, Bob Peterson, David Reynolds", daoActores.getActores(), "After his son is captured in the Great Barrier Reef and taken to Sydney, a timid clownfish sets out on a journey to bring him home.", "English","17", "USA", R.drawable.movie1infantiles, "8,1/10", "90", "5",1, "Film", R.drawable.findingnemo));
+        peliculasInfantiles.add(new Pelicula("High School Musical", "2006", "PG", "20-01-2006", "98 min","Musical","Kenny Ortega","Peter Barsocchini",daoActores.getActores(),"A popular high school athlete and an academically gifted girl get roles in the school musical and develop a friendship that threatens East High's social order.","English", "9","USA", R.drawable.movie2infantiles, "5,3/10","N/A", "3", 2, "Film", R.drawable.hsmtrailer));
+        peliculasInfantiles.add(new Pelicula("Maleficent", "2014", "PG", "28-05-2014","97 min","Dark Fantasy","Robert Stromberg","Linda Woolverton", daoActores.getActores(),"A vengeful fairy is driven to curse an infant princess, only to discover that the child may be the one person who can restore peace to their troubled land.","English", "10","USA", R.drawable.movie3infantiles,"7/10","56","8",3,"Film", R.drawable.maleficenttrailer));
+        peliculasInfantiles.add(new Pelicula("Los Increibles 2", "2018","G","05-06-2018","118 min", "Animation","Brad Bird","Brad Bird",daoActores.getActores(),"Bob Parr (Mr. Incredible) is left to care for the kids while Helen (Elastigirl) is out saving the world.","English","3","USA",  R.drawable.movie4infantiles,"7,9/10","80","8",4, "Film", R.drawable.incrediblestwo));
+        peliculasInfantiles.add(new Pelicula("Los Increibles","2004","G","27-10-2004","115 min", "Animation","Brad Bird","Brad Bird", daoActores.getActores(),"A family of undercover superheroes, while trying to live the quiet suburban life, are forced into action to save the world.","English","67", "USA", R.drawable.movie5infantiles, "8/10","90","9",5,"Film", R.drawable.incrediblestrailer));
+        peliculasInfantiles.add(new Pelicula("Intensamente","2015", "G","18-05-2015","94 min","Animation","Pete Docter","Pete Docter, Ronnie Del Carmen", daoActores.getActores(),"After young Riley is uprooted from her Midwest life and moved to San Francisco, her emotions - Joy, Fear, Anger, Disgust and Sadness - conflict on how best to navigate a new city, house, and school.","English","94","USA",R.drawable.movie6infantiles,"8,2/10","94","8",6,"Film", R.drawable.insideouttrailer));
+        peliculasInfantiles.add(new Pelicula("Juego de Gemelas",R.drawable.movie7infantiles));
         peliculasInfantiles.add(new Pelicula("Mini Espeias", 1,R.drawable.movie8infantiles));
         peliculasInfantiles.add(new Pelicula("Tangled", 1,R.drawable.movie9infantiles));
         peliculasInfantiles.add(new Pelicula("Coco", 1,R.drawable.movie10infantiles));
